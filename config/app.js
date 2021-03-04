391fda7d2ab142d3c3552826d0257cc4c33ed93c
Fabián Olivares Ríos <f.olivaresrios@gmail.com>
2021-03-04 20:49:51 -0300

    Create our first middleware and install morgan

---
+++ config/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import morgan from 'morgan';
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+import bootcamps from '../routes/bootcamps.js';
+
+//Settings
+app.set('port', port);
+
+//middlewares
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+//Route files
+app.use('/api/v1/bootcamps', bootcamps);
+
+export default app;
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Read environment variables
 import dotenv from 'dotenv';
-import app from './app.js';
+import app from './config/app.js';
 
 dotenv.config();
+
 // Server is listening
 app.listen(app.get('port'), () => {
   console.log('Server on port', app.get('port'));
